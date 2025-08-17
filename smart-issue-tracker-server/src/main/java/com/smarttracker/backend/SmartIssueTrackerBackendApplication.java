4f34faec09ae5b88eb5f2f0495bee32128c4ccaf
evangelospap <papagiannopoulos.evan@gmail.com>
2025-08-17 17:02:49 +0300

    added backend Spring Boot + Grandle-Groovy

---
+++ smart-issue-tracker-server/src/main/java/com/smarttracker/backend/SmartIssueTrackerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.smarttracker.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartIssueTrackerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartIssueTrackerBackendApplication.class, args);
+	}
+
+}
---
+++ smart-issue-tracker-server/src/test/java/com/smarttracker/backend/SmartIssueTrackerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smarttracker.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartIssueTrackerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
